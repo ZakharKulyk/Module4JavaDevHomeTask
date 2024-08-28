1c39201c26537ee264ecb1a9432d68c05597420d
zkhr <zakhar864@gmail.com>
2024-08-28 13:57:26 +0200

    Db

---
+++ print_project_prices.sql
@@ -0,0 +1,9 @@
+SELECT p.project_id                                                          AS PROJECT_ID,
+       ROUND(SUM(w.salary) * DATEDIFF('MONTH', p.start_date, p.finish_date)) AS PRICE
+FROM project p
+         JOIN
+     project_worker pw ON p.project_id = pw.project_id
+         JOIN
+     worker w ON pw.worker_id = w.id
+GROUP BY p.project_id, p.start_date, p.finish_date
+ORDER BY PRICE DESC;
